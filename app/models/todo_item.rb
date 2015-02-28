90754914430d7f27e371218fde97c39666a8602c
kennytrionfo <kentrionfo@gmail.com>
2015-02-28 06:55:18 -0700

    added completed at mark through styling

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,9 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
+
+  def completed? # If it's completed, the completed_at field should not be blank. 
+       !completed_at.blank?    
+  end
+
+  
 end
