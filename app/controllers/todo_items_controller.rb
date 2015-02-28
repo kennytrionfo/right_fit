d0694cf00bb7a5784153ce01bb9a2d6f9d300cf9
kennytrionfo <kentrionfo@gmail.com>
2015-02-28 06:47:53 -0700

    added flash message for complete action

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,14 +1,19 @@
 class TodoItemsController < ApplicationController
 
-    before_action :set_todo_list
+    before_action :set_todo_list 
+    before_action :set_todo_item, except: [:create]
 
     def create
     	@todo_item = @todo_list.todo_items.create(todo_item_params)
     	redirect_to @todo_list
     end
+
+    def complete
+        @todo_item.update_attribute(:completed_at, Time.now)
+        redirect_to @todo_list, notice: "Todo item completed dude"
+    end
     	
     def destroy
-        @todo_item = @todo_list.todo_items.find(params[:id])
         if @todo_item.destroy
             flash[:succss] = "Todo List item deleted dude"
         else
@@ -25,6 +30,10 @@ class TodoItemsController < ApplicationController
 
     def todo_item_params
     	params[:todo_item].permit(:content)    	
+    end
+
+    def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
     end
 
 end
